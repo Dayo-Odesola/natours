0d3da203bb13e6b875cbef9de8aa842caebf2317
Dayo-Odesola <dayoodesola2001@gmail.com>
2022-09-12 20:22:43 +0100

    api features: Pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -27,7 +27,7 @@ exports.getAllTours = async (req, res) => {
       const sortBy = req.query.sort.split(',').join(' ')
       query = query.sort(sortBy)
     } else {
-      query = query.sort('-createdAt')
+      // query = query.sort('-createdAt')
     }
 
 
@@ -39,7 +39,19 @@ exports.getAllTours = async (req, res) => {
     } else {
       query = query.select('-__v')
     }
-    
+
+    // 4) pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit)
+
+    if(req.query.page) {
+      const numTours = await Tour.countDocuments()
+
+      if(skip >= numTours) throw new Error('Tis page does not exist')
+    }
   
     
     
