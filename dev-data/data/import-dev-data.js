aa95af0c76b5c05a4f8f5892da450590469406b1
Dayo-Odesola <dayoodesola2001@gmail.com>
2022-09-12 18:30:44 +0100

    importing dev data

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -18,3 +18,39 @@ mongoose
     useUnifiedTopology: true,
   })
   .then((con) => console.log('DB connection successful!'));
+
+  // READ JSON FILE
+  const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'))
+
+
+  // IMPORT DATA IN TO DB
+  const importData = async () => {
+    try {
+      await Tour.create(tours)
+      console.log('Data successfully loaded!')
+    }catch(err) {
+      console.log(err)
+    }
+
+    process.exit()
+  }
+
+
+  // DELETE ALL DATA FROM COLLECTION
+  const deleteData = async () => {
+    try {
+      await Tour.deleteMany()
+      console.log('Data successfully deleted!')
+    }catch(err) {
+      console.log(err)
+    }
+
+    process.exit()
+  }
+
+  if(process.argv[2] === '--import') {
+    importData()
+  }else if (process.argv[2] === '--delete') {
+    deleteData()
+  }
+  console.log(process.argv)
